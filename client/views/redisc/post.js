109ee712a3e519caf2b922d9a87f186a9839e6ae
mhhf <mhhf@freenet.de>
2014-08-02 17:16:29 +0200

    [+] restructurate js file structure

--- client/views/redisc/post.js
+++ client/views/redisc/post.js
@@ -1,15 +1,3 @@
-
-Template.RediscPostNew.events = {
-  "click .save-btn": function(e,t){
-    var atom = _.extend( new LLMD.Atom('redisc'), this.buildAtom() );
-    var _atomId = Atoms.insert(atom);
-    Meteor.call('atom.compile', _atomId);
-    Router.go('RediscPostView', {
-      _id: _atomId
-    });
-  }
-}
-
 var rediscModel;
 
 Template.RediscPostView.helpers({
@@ -84,48 +72,5 @@ Template.Comments.helpers({
     children.sort( function( a, b ){
       return b.get().score - a.get().score; });
     return children;
-  }
-});
-
-
-Template.RediscPosts.helpers({
-  atomModel: function(){
-    return new AtomModel( this._id );
-  },
-  getTags: function(){
-    return Session.get('tags');
-  },
-  getPageUrl: function( ctx ){
-    if(ctx.tags && ctx.tags.length > 0) {
-      return '/tags/'+ctx.tags.join(',')+'/'+this;
-    } else {
-      return '/all/'+this;
-    }
-  },
-  getNextUrl: function(){
-    if(this.tags && this.tags.length > 0) {
-      return '/tags/'+this.tags.join(',')+'/'+(this.page+1);
-    } else {
-      return '/all/'+(this.page+1);
-    }
-  },
-  getPrevUrl: function(){
-    if(this.tags && this.tags.length > 0) {
-      return '/tags/'+this.tags.join(',')+'/'+(this.page-1);
-    } else {
-      return '/all/'+(this.page-1);
-    }
-  },
-  hasPrevious: function(){
-    return this.page>0;
-  },
-  hasNext: function(){
-    return this.pages.length-1 > this.page;
-  },
-  isActive: function( ctx ){
-    return ctx.page == this?'active':'';
-  },
-  hasPages: function(){
-    return this.pages.length > 1
   }
 });
