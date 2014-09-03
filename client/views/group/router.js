6bcf8a48b9f8829ad05950075ff2eea92e6dc6fe
mhhf <mhhf@freenet.de>
2014-09-03 10:34:10 +0200

    [+] group template

--- client/views/group/router.js
+++ client/views/group/router.js
@@ -1,5 +1,17 @@
 Router.map( function(){
   
+  this.route('group', {
+    path: '/group/:_id',
+    waitOn: function(){
+      return Meteor.subscribe('group', this.params._id);
+    },
+    data: function(){
+      return { 
+        group: Owners.findOne({ _id: this.params._id })
+      }
+    }
+  });
+  
   this.route('groupTransact', {
     path: '/group/:_id/transact',
     waitOn: function(){
